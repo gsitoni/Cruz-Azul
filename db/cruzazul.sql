448a0fa11d2b7358b3292ffe6befe94e0eb9e8d5
gaby <gabrielly4039@gmail.com>
2026-04-14 11:49:38 -0300

    Organização dos diretórios

--- db/cruzazul.sql
+++ db/cruzazul.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Tempo de geração: 14-Abr-2026 às 00:22
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
+-- Host: 127.0.0.1
+-- Tempo de geração: 14/04/2026 às 14:10
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,24 +45,40 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `beneficiario`
+-- Estrutura para tabela `beneficiario`
 --
 
 CREATE TABLE `beneficiario` (
   `id_beneficiario` int(10) UNSIGNED NOT NULL,
-  `nome_receptor` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cnpj` char(18) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `localizacao` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `classificacao_risco` enum('emergencia','continuo','pontual','baixa_prioridade') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status_elegibilidade` enum('ativo','suspenso','inativo') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'ativo',
+  `nome_receptor` varchar(300) NOT NULL,
+  `cnpj` char(18) NOT NULL,
+  `localizacao` varchar(50) NOT NULL,
+  `classificacao_risco` enum('emergencia','continuo','pontual','baixa_prioridade') NOT NULL,
+  `status_elegibilidade` enum('pendente','aprovada','rejeitada') DEFAULT 'pendente',
   `data_atualizacao` datetime NOT NULL DEFAULT current_timestamp(),
-  `criado_em` datetime NOT NULL DEFAULT current_timestamp()
+  `criado_em` datetime NOT NULL DEFAULT current_timestamp(),
+  `sigla_estado` varchar(2) DEFAULT NULL,
+  `endereco` varchar(200) DEFAULT NULL,
+  `cidade` varchar(50) DEFAULT NULL,
+  `descricao` varchar(300) DEFAULT NULL,
+  `senha_hash` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `token_confirmacao` varchar(200) DEFAULT NULL,
+  `area_atuacao` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `beneficiario`
+--
+
+INSERT INTO `beneficiario` (`id_beneficiario`, `nome_receptor`, `cnpj`, `localizacao`, `classificacao_risco`, `status_elegibilidade`, `data_atualizacao`, `criado_em`, `sigla_estado`, `endereco`, `cidade`, `descricao`, `senha_hash`, `email`, `token_confirmacao`, `area_atuacao`) VALUES
+(3, 'Abrigo Esperança', '15.672.083/0001-32', '59797-199', 'emergencia', '', '2026-04-11 11:22:07', '2026-04-11 11:22:07', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 'Bismark Otto', '12345678000190', '82560370', 'emergencia', 'pendente', '2026-04-13 10:37:29', '2026-04-13 10:37:29', 'PR', 'Rua Ary Barroso', 'Curitiba', 'aaa', '$2y$10$ZUpDIrUrov0vCZVTjbb.XuNcsI3qvw2RlYnP/o0NYNTzAmdNIh.2u', 'beargamessirwhiter25@gmail.com', '039d7b1291708f162158ce1ec0addc3e', 'Alimentação');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `distribuicao`
+-- Estrutura para tabela `distribuicao`
 --
 
 CREATE TABLE `distribuicao` (
@@ -74,7 +90,14 @@ CREATE TABLE `distribuicao` (
   `data_hora` datetime NOT NULL DEFAULT current_timestamp(),
   `comprovante_url` text NOT NULL,
   `criado_em` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ;
+
+--
+-- Despejando dados para a tabela `distribuicao`
+--
+
+INSERT INTO `distribuicao` (`id_operacao`, `id_lote`, `id_beneficiario`, `id_voluntario`, `quantidade_retirada`, `data_hora`, `comprovante_url`, `criado_em`) VALUES
+(6, 2, 3, 3, 1.847, '2026-04-11 11:25:50', 'comprovantes/dist-001.pdf', '2026-04-11 11:25:50');
 
 --
 -- Acionadores `distribuicao`
@@ -130,7 +153,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `doacao`
+-- Estrutura para tabela `doacao`
 --
 
 CREATE TABLE `doacao` (
@@ -143,9 +166,15 @@ CREATE TABLE `doacao` (
   `data_validade` date DEFAULT NULL,
   `estado_conservacao` enum('novo','usado','desgastado') DEFAULT NULL,
   `data_doacao` date NOT NULL,
-  `criado_em` datetime NOT NULL DEFAULT current_timestamp(),
-  `STATUS` varchar(20) DEFAULT 'pendente'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `criado_em` datetime NOT NULL DEFAULT current_timestamp()
+) ;
+
+--
+-- Despejando dados para a tabela `doacao`
+--
+
+INSERT INTO `doacao` (`id_doacao`, `id_doador`, `categoria`, `item`, `quantidade`, `unidade_medida`, `data_validade`, `estado_conservacao`, `data_doacao`, `criado_em`) VALUES
+(3, 3, 'alimento', 'Óleo de soja', 48.683, 'unidade', '2026-07-13', NULL, '2025-09-30', '2026-04-11 11:22:45');
 
 --
 -- Acionadores `doacao`
@@ -211,23 +240,31 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `doador`
+-- Estrutura para tabela `doador`
 --
 
 CREATE TABLE `doador` (
   `id_doador` int(10) UNSIGNED NOT NULL,
-  `cpf_cnpj` varchar(18) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nome` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cpf_cnpj` varchar(18) NOT NULL,
+  `nome` varchar(200) NOT NULL,
   `data_nascimento` date DEFAULT NULL,
-  `telefone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `email` varchar(254) NOT NULL,
   `criado_em` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `doador`
+--
+
+INSERT INTO `doador` (`id_doador`, `cpf_cnpj`, `nome`, `data_nascimento`, `telefone`, `email`, `criado_em`) VALUES
+(2, '123.456.789-00', 'Maria Silva', '1990-05-15', '11987654321', 'maria@email.com', '2026-04-11 11:15:17'),
+(3, '938.761.450-66', 'Cecília Ramos', '1997-04-21', '11929958838', 'cecília.ramos143@uol.com.br', '2026-04-11 11:21:44');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `estoque`
+-- Estrutura para tabela `estoque`
 --
 
 CREATE TABLE `estoque` (
@@ -241,44 +278,71 @@ CREATE TABLE `estoque` (
   `status_operacional` enum('disponivel','quarentena','avariado','vencido','esgotado') NOT NULL DEFAULT 'disponivel',
   `criado_em` datetime NOT NULL DEFAULT current_timestamp(),
   `atualizado_em` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ;
+
+--
+-- Despejando dados para a tabela `estoque`
+--
+
+INSERT INTO `estoque` (`id_lote`, `codigo_lote`, `id_doacao`, `item`, `quantidade_atual`, `localizacao_fisica`, `data_validade`, `status_operacional`, `criado_em`, `atualizado_em`) VALUES
+(2, 'LOT-20260411-0003', 3, 'Óleo de soja', 46.836, 'A DEFINIR', '2026-07-13', 'disponivel', '2026-04-11 11:22:45', '2026-04-11 11:25:50');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Estrutura para tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
   `id_usuario` int(10) UNSIGNED NOT NULL,
-  `email` varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `senha_hash` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status_cadastro` enum('pendente','confirmado','bloqueado') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pendente',
-  `token_confirmacao` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `token_recuperacao` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nome` varchar(200) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `senha_hash` text NOT NULL,
+  `status_cadastro` enum('pendente','confirmado','bloqueado') NOT NULL DEFAULT 'pendente',
+  `token_confirmacao` varchar(255) DEFAULT NULL,
+  `token_recuperacao` varchar(255) DEFAULT NULL,
   `token_expira_em` datetime DEFAULT NULL,
-  `chave_2fa` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `data_criacao` datetime NOT NULL DEFAULT current_timestamp()
+  `chave_2fa` varchar(64) DEFAULT NULL,
+  `data_criacao` datetime NOT NULL DEFAULT current_timestamp(),
+  `permissao` enum('Doador','Admin') DEFAULT 'Doador'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nome`, `email`, `senha_hash`, `status_cadastro`, `token_confirmacao`, `token_recuperacao`, `token_expira_em`, `chave_2fa`, `data_criacao`, `permissao`) VALUES
+(1, '', 'usuario1655@terra.com.br', 'a2ca37fe6fdc490b8f7ce841e1701a169d2b1697c6b5b5c63f94abb8f9b6d6dd', 'confirmado', NULL, NULL, NULL, NULL, '2026-04-11 11:21:32', NULL),
+(2, '', 'teste@teste.com', '$2y$10$35jJlOeRUIRvNodUNE3QNu0Bds70sgshRhCGaoRf3rWYHvwMRsW3S', 'confirmado', NULL, NULL, NULL, NULL, '2026-04-11 21:09:44', NULL),
+(3, 'nome', 'ablu@gmail.com', '$2y$10$lFtaI.y4kU5wlO7sdJs4beD2iLlRSje41ATpdcVdsoEhrDKkG1VkO', 'pendente', '8062f5936f559de19f6b20e2d916798f6119261e71ad5feab8287d05ceca70b5', NULL, NULL, NULL, '2026-04-12 14:54:31', NULL),
+(5, 'nome', 'email@teste.com', '$2y$10$S4Q4Je8gRKoNrkcNUoHRSOQ9A2gaqt3lyrxyE1LAwU49plENZN/5G', 'pendente', 'a412968ba7042df1848118e531f1213f52c9be769ded05b75a16871009c69543', NULL, NULL, NULL, '2026-04-12 15:01:30', NULL),
+(28, 'nome', 'beargamessirwhiter25@gmail.com', '$2y$10$5.aO.FBEKYW98ZQuHC4ba.CEeBSviPXBuwA6Ule3iGxztAhCy9kyS', 'pendente', 'b73b16df44cc3dff253dc8c3999d509490461f3dd172229e4173e0aa622f23fd', NULL, NULL, NULL, '2026-04-14 08:42:57', 'Doador');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `voluntario`
+-- Estrutura para tabela `voluntario`
 --
 
 CREATE TABLE `voluntario` (
   `id_voluntario` int(10) UNSIGNED NOT NULL,
-  `nome` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cpf` char(14) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telefone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `funcao` enum('recepcionista','motorista','almoxarife','assistente_social','coordenador') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `disponibilidade` enum('dias_uteis','fins_de_semana','ambos') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nome` varchar(200) NOT NULL,
+  `cpf` char(14) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `funcao` enum('recepcionista','motorista','almoxarife','assistente_social','coordenador') NOT NULL,
+  `disponibilidade` enum('dias_uteis','fins_de_semana','ambos') NOT NULL,
   `data_entrada` date NOT NULL,
-  `status_operacional` enum('ativo','inativo','suspenso') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'ativo',
+  `status_operacional` enum('ativo','inativo','suspenso') NOT NULL DEFAULT 'ativo',
   `criado_em` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `voluntario`
+--
+
+INSERT INTO `voluntario` (`id_voluntario`, `nome`, `cpf`, `telefone`, `email`, `funcao`, `disponibilidade`, `data_entrada`, `status_operacional`, `criado_em`) VALUES
+(3, 'Ryan Mendonça', '052.481.973-41', '11973140807', 'ryan.mendonça430@uol.com.br', 'recepcionista', 'dias_uteis', '2024-01-16', 'ativo', '2026-04-11 11:21:58');
 
 --
 -- Acionadores `voluntario`
@@ -305,7 +369,7 @@ DELIMITER ;
 --
 
 --
--- Índices para tabela `beneficiario`
+-- Índices de tabela `beneficiario`
 --
 ALTER TABLE `beneficiario`
   ADD PRIMARY KEY (`id_beneficiario`),
@@ -313,7 +377,7 @@ ALTER TABLE `beneficiario`
   ADD KEY `idx_beneficiario_status` (`status_elegibilidade`);
 
 --
--- Índices para tabela `distribuicao`
+-- Índices de tabela `distribuicao`
 --
 ALTER TABLE `distribuicao`
   ADD PRIMARY KEY (`id_operacao`),
@@ -322,14 +386,14 @@ ALTER TABLE `distribuicao`
   ADD KEY `idx_distribuicao_vol` (`id_voluntario`);
 
 --
--- Índices para tabela `doacao`
+-- Índices de tabela `doacao`
 --
 ALTER TABLE `doacao`
   ADD PRIMARY KEY (`id_doacao`),
   ADD KEY `idx_doacao_doador` (`id_doador`);
 
 --
--- Índices para tabela `doador`
+-- Índices de tabela `doador`
 --
 ALTER TABLE `doador`
   ADD PRIMARY KEY (`id_doador`),
@@ -338,7 +402,7 @@ ALTER TABLE `doador`
   ADD UNIQUE KEY `uq_doador_email` (`email`);
 
 --
--- Índices para tabela `estoque`
+-- Índices de tabela `estoque`
 --
 ALTER TABLE `estoque`
   ADD PRIMARY KEY (`id_lote`),
@@ -348,14 +412,14 @@ ALTER TABLE `estoque`
   ADD KEY `idx_estoque_validade` (`data_validade`);
 
 --
--- Índices para tabela `usuario`
+-- Índices de tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_usuario`),
   ADD UNIQUE KEY `uq_usuario_email` (`email`);
 
 --
--- Índices para tabela `voluntario`
+-- Índices de tabela `voluntario`
 --
 ALTER TABLE `voluntario`
   ADD PRIMARY KEY (`id_voluntario`),
@@ -365,14 +429,14 @@ ALTER TABLE `voluntario`
   ADD KEY `idx_voluntario_status` (`status_operacional`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `beneficiario`
 --
 ALTER TABLE `beneficiario`
-  MODIFY `id_beneficiario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_beneficiario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `distribuicao`
@@ -390,7 +454,7 @@ ALTER TABLE `doacao`
 -- AUTO_INCREMENT de tabela `doador`
 --
 ALTER TABLE `doador`
-  MODIFY `id_doador` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_doador` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `estoque`
@@ -402,20 +466,20 @@ ALTER TABLE `estoque`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de tabela `voluntario`
 --
 ALTER TABLE `voluntario`
-  MODIFY `id_voluntario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_voluntario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Restrições para despejos de tabelas
+-- Restrições para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `distribuicao`
+-- Restrições para tabelas `distribuicao`
 --
 ALTER TABLE `distribuicao`
   ADD CONSTRAINT `fk_dist_beneficiario` FOREIGN KEY (`id_beneficiario`) REFERENCES `beneficiario` (`id_beneficiario`),
@@ -423,13 +487,13 @@ ALTER TABLE `distribuicao`
   ADD CONSTRAINT `fk_dist_voluntario` FOREIGN KEY (`id_voluntario`) REFERENCES `voluntario` (`id_voluntario`);
 
 --
--- Limitadores para a tabela `doacao`
+-- Restrições para tabelas `doacao`
 --
 ALTER TABLE `doacao`
   ADD CONSTRAINT `fk_doacao_doador` FOREIGN KEY (`id_doador`) REFERENCES `doador` (`id_doador`);
 
 --
--- Limitadores para a tabela `estoque`
+-- Restrições para tabelas `estoque`
 --
 ALTER TABLE `estoque`
   ADD CONSTRAINT `fk_estoque_doacao` FOREIGN KEY (`id_doacao`) REFERENCES `doacao` (`id_doacao`);
--- db/setup_db.bat
+++ db/setup_db.bat
@@ -6,13 +6,13 @@ echo ================================
 REM ===== CONFIGURAÇÕES =====
 set MYSQL_USER=root
 set MYSQL_PASSWORD=
-set MYSQL_PATH="C:\xampp\mysql\bin\mysql.exe"
+set MYSQL_PATH=C:\xampp\mysql\bin\mysql.exe
 set SQL_FILE=cruzazul.sql
 
 echo.
 echo Criando banco de dados...
 
-%MYSQL_PATH% -u %MYSQL_USER% -p%MYSQL_PASSWORD% -e "CREATE DATABASE IF NOT EXISTS cruzazul CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+"%MYSQL_PATH%" -u "%MYSQL_USER%" -p%MYSQL_PASSWORD% -e "CREATE DATABASE IF NOT EXISTS cruzazul CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 
 IF %ERRORLEVEL% NEQ 0 (
     echo ERRO ao criar banco.
